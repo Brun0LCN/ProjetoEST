4dc0057cdaef6c313fd0e50b50ea1ba258b95728
Brun0LCN <kaysam878@gmail.com>
2024-07-11 16:35:20 -0300

    Create Doc_Teste.py

---
+++ Doc_Teste.py
@@ -0,0 +1,79 @@
+# print(type('Hello, world 40, 46'), 40, sep='-')
+# print(type(11)) # int
+# # comentarios
+# print(type(2.3))
+# print(10 == 10)
+# print(int(10 == 11))
+
+# nome = 'Bruno'
+# idade = 20
+# maior_de_idade = idade >= 18
+# Altura = 1.80
+# ano_nascimento = 2024 - idade 
+
+# print('nome:', nome)
+# print('idade:', idade)
+# print('Anos de nascimento', ano_nascimento)
+# print('Mario de idade?', maior_de_idade)
+# print('Altura:', Altura)
+
+# adicao = 10 + 10
+# print('Adição', adicao)
+
+# subtracao = 10 - 5
+# print('Subtração', subtracao)
+
+# multiplicacao = 10 * 10
+# print('Multiplicação', multiplicacao)
+
+# divisao = 10 / 3  # float
+# print('Divisão', divisao)
+
+# divisao_inteira = 10 // 3
+# print('Divisão inteira', divisao_inteira)
+
+# exponenciacao = 2 ** 10
+# print('Exponenciação', exponenciacao)
+
+# modulo = 55 % 2  # resto da divisão
+# print('Módulo', modulo)
+
+# print(10 % 8 == 0)
+# print(16 % 8 == 0)
+# print(10 % 2 == 0)
+# print(15 % 2 == 0)
+# print(16 % 2 == 0)
+
+# peso = 92
+# altura = 1.80
+# imc = peso / altura ** 2
+# nome = 'bruno'    
+
+# print(nome, 'tem', altura, 'de altura')
+# print('pesa', peso, 'quilos e seu IMC é')
+# print(int(imc))
+
+# if / elif      / else
+# se / se não se / se não
+
+# entrada = input('Você quer "entrar" ou "sair"? ')
+
+# if entrada == 'entrar':
+#     print('Você entrou no sistema')
+
+#     print(12341234)
+# elif entrada == 'sair':
+#     print('Você saiu do sistema')
+# else:
+#     print('Você não digitou nem entrar e nem sair.')
+
+# print('FORA DOS BLOCOS')
+
+primeiro_valor = input('digite um valor:')
+segundo_valor = input('digite outro valor:')
+
+if primeiro_valor > segundo_valor:
+    print('Primeiro valor',primeiro_valor,'é mairo que o segundovalor',segundo_valor)
+
+else :
+    print('Segundo valor',segundo_valor,'é mairo que o primeiro1valor',primeiro_valor)
